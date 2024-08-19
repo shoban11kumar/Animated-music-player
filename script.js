3df87fb5dbdf00477b7a5e3b171fe347d113b76f
shoban11kumar <124165188+shoban11kumar@users.noreply.github.com>
2024-08-19 10:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+
+const songs = [
+  {
+    title: "Naa ready",
+    name: "leo",
+    source:
+      "songs/song1 Naa ready.mp3",
+  },
+  {
+    title: "Badass",
+    name: "Leo",
+    source:
+      "songs/Song 2 badass.mp3",
+  },
+  {
+    title: "Vaathi coming",
+    name: "Master",
+    source:
+      "songs/Song3 vaathi.mp3",
+  },
+  {
+    title: "Selfie pulla",
+    name: "Kaththi",
+    source:
+      "songs/Song 4 selfie.mp3",
+  },
+  {
+    title: "Aathi",
+    name: "kaththi",
+    source:
+      "songs/song5 aathi.mp3",
+  },
+
+  {
+    title: "Whistle podu",
+    name: "The Greatest Of All Time",
+    source:
+      "songs/song6 wistle podu.mp3",
+  },
+  {
+    title: "Spark",
+    name: "The Greatest Of All Time",
+    source:
+      "songs/Song 7 spark.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
